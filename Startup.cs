f7fe860dbe96d08656f4a48d7a1cbca19250795a
Andrei <aandrei.stanimir@gmail.com>
2020-12-04 14:13:26 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chess20.Startup))]
+namespace Chess20
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
