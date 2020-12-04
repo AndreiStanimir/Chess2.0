d46822cd3390e631fa0a3628139f0abc7ba671ad
Andrei <aandrei.stanimir@gmail.com>
2020-12-04 23:08:06 +0200

    change default connection string

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Chess20.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ChessDBConnectionString", throwIfV1Schema: false)
         {
         }
 
