3cdb353768bcf0ad97b9d95f4dbeff3fa9543030
Andrei <aandrei.stanimir@gmail.com>
2021-01-01 23:34:49 +0200

    Add puzzle view

---
+++ Controllers/PuzzlesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chess20.Controllers
+{
+    public class PuzzlesController : Controller
+    {
+        // GET: Puzzles
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
