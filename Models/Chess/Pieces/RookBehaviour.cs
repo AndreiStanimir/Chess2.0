37db2d9e13be8306e6d09db273c93bfe0b783130
Andrei <aandrei.stanimir@gmail.com>
2021-02-09 23:05:52 +0200

    Add rook behaviour

---
+++ Models/Chess/Pieces/RookBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using Chess20.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chess20.Models.Chess.Pieces
+{
+    public class RookBehaviour
+    {
+        public static Move[] GetMoves(BoardTiles tiles, Position position)
+        {
+            var moves = new List<Move>(9);
+
+            moves.AddRange(GetMovesWithOffsets(tiles, position, 1, 0));
+            moves.AddRange(GetMovesWithOffsets(tiles, position, 0, -1));
+            moves.AddRange(GetMovesWithOffsets(tiles, position, -1, 0));
+            moves.AddRange(GetMovesWithOffsets(tiles, position, 0, 1));
+
+            return moves.ToArray();
+        }
+
+        private static List<Move> GetMovesWithOffsets(BoardTiles tiles, Position position, int offsetX, int offsetY)
+        {
+            var moves = new List<Move>(9);
+            for (int y = position.Y, x = position.X; CONSTANTS.IsInside(y, x); y += offsetY, x += offsetX)
+            {
+                moves.Add(Factory.GetMove(tiles[position], tiles[y, x]));
+
+                if (tiles[y, x]?.IsEmpty() == false)
+                {
+                    return moves;
+                }
+            }
+            return moves;
+        }
+    }
+}
